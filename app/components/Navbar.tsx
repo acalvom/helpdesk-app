dc9b70d05b3bf3268bd292bc71659cf2d569421a
acalvom <andreac.3108@gmail.com>
2023-10-10 08:41:56 +0200

    style: add styles

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,13 +2,13 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <nav>
-      <h1>HELP DESK</h1>
-      <ul>
-        <li>
+    <nav className="flex flex-row w-full mx-4 my-8">
+      <h1 className="px-2 py-1 text-lg text-primary-extra-dark font-bold ">HELP DESK</h1>
+      <ul className="flex flex-row mx-auto gap-4">
+        <li className="px-2 py-1 text-lg text-primary-dark font-semibold rounded-md hover:bg-primary-hover hover:text-primary-extra-light">
           <Link href="/"> Dashboard </Link>
         </li>
-        <li>
+        <li className="px-2 py-1 text-lg text-primary-dark font-semibold rounded-md hover:bg-primary-hover  hover:text-primary-extra-light">
           <Link href="/tickets"> Tickets </Link>
         </li>
       </ul>
