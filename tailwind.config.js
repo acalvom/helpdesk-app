e1c8c29f1d9bb9f8e8cf2e1049712ca9acdff061
acalvom <andreac.3108@gmail.com>
2023-10-10 08:41:13 +0200

    style: add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,29 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'primary-extra-light': '#f2f9fd',
+        'primary-light': '#c5e3f2',
+        'primary-main': '#8ecae6',
+        'primary-hover': '#58b2d8',
+        'primary-focus': '#3298c5',
+        'primary-dark': '#1d6187',
+        'primary-extra-dark': '#1c465e',
+        'secondary-light': '#fff9c5',
+        'secondary-main': '#fff485',
+        'secondary-hover': '#ffe746',
+        'secondary-focus': '#ffd71b',
+        'secondary-dark': '#ffb703',
+        'secondary-extra-dark': '#bb6202',
+        'success-light': '#f0fe58',
+        'success-main': '#c3dc06',
+        'success-dark': '#a5be00',
+        'warning-light': '#ffc36d',
+        'warning-main': '#ff9e32',
+        'warning-dark': '#cc4902',
+        'error-light': '#ff8b8b',
+        'error-main': '#ff4949',
+        'error-dark': '#be0000',
       },
     },
   },
