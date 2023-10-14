05203383a405cc6ee16c13a4e23d280c0e036e04
acalvom <andreac.3108@gmail.com>
2023-10-14 19:02:24 +0200

    feat: add Ticket list

---
+++ app/tickets/TicketList.tsx
@@ -0,0 +1,39 @@
+import { Ticket } from '../models/Ticket'
+
+const getTickets = async () => {
+  const res = await fetch('http://localhost:4000/tickets')
+  return res.json()
+}
+
+const colorMap: any = {
+  low: 'bg-success-main',
+  medium: 'bg-warning-main',
+  high: 'bg-error-main',
+}
+
+export default async function TicketList() {
+  const tickets = await getTickets()
+
+  return (
+    <>
+      {tickets.map((ticket: Ticket) => (
+        <div
+          key={ticket.id}
+          className=" mb-4 bg-primary-extra-light rounded-xl border-2 border-green-main drop-shadow-lg"
+        >
+          <h3 className="p-4 text-md text-green-main font-semibold">{ticket.title}</h3>
+          <p className="p-4 text-md text-green-dark">{ticket.body.slice(0, 400)}...</p>
+          <div className="flex flex-row-reverse">
+            <span
+              className={`px-4 py-2 text-xs text-green-dark font-semibold uppercase rounded-tl-lg rounded-br-lg ${
+                colorMap[ticket.priority]
+              }`}
+            >
+              {ticket.priority}
+            </span>
+          </div>
+        </div>
+      ))}
+    </>
+  )
+}
--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,7 +1,10 @@
+import TicketList from './TicketList'
+
 export default function Tickets() {
   return (
-    <main>
-      <h2>Tickets</h2>
+    <main className="flex flex-col m-16">
+      <h2 className="py-2 text-lg text-yellow-main font-bold">Tickets</h2>
+      <TicketList />
     </main>
   )
 }
