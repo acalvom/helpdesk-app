a19f81eddebf4097ba2173dbf764ede7586d8191
acalvom <andreac.3108@gmail.com>
2023-10-14 18:12:50 +0200

    style: custom styles in navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,16 +2,16 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <nav className="flex flex-row w-full mx-4 my-8">
-      <h1 className="px-2 py-1 text-lg text-primary-extra-dark font-bold ">HELP DESK</h1>
-      <ul className="flex flex-row mx-auto gap-4">
-        <li className="px-2 py-1 text-lg text-primary-dark font-semibold rounded-md hover:bg-primary-hover hover:text-primary-extra-light">
-          <Link href="/"> Dashboard </Link>
-        </li>
-        <li className="px-2 py-1 text-lg text-primary-dark font-semibold rounded-md hover:bg-primary-hover  hover:text-primary-extra-light">
-          <Link href="/tickets"> Tickets </Link>
-        </li>
-      </ul>
+    <nav className="flex max-w-screen border-b-2 border-green-dark m-4">
+        <h1 className="hidden p-3 text-xl text-green-dark font-bold sm:block">HELP DESK</h1>
+        <ul className="flex sm:flex-row flex-wrap mx-auto gap-4 content-center">
+          <li className="p-2 text-lg text-green-dark font-semibold rounded-md hover:bg-green-main hover:text-primary-extra-light">
+            <Link href="/"> Dashboard </Link>
+          </li>
+          <li className="p-2 text-lg text-green-dark font-semibold rounded-md hover:bg-green-main  hover:text-primary-extra-light">
+            <Link href="/tickets"> Tickets </Link>
+          </li>
+        </ul>
     </nav>
   )
 }
