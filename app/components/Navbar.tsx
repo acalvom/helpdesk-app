19154fbf8c44c154fa551415e98b2c243ce4d123
acalvom <andreac.3108@gmail.com>
2023-10-15 05:59:12 +0200

    feat: add app icons

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,12 @@
+import Image from 'next/image'
 import Link from 'next/link'
+import Helpdesk from '../assets/logo.png'
 
 export default function Navbar() {
   return (
     <nav className="flex max-w-screen border-b-2 border-green-dark m-4">
-        <h1 className="hidden p-3 text-xl text-green-dark font-bold sm:block">HELP DESK</h1>
-        <ul className="flex sm:flex-row flex-wrap mx-auto gap-4 content-center">
+        <Image src={Helpdesk} alt='helpdesk' height={70} className="hidden sm:block"/>
+        <ul className="flex sm:flex-row flex-wrap gap-4 content-center">
           <li className="p-2 text-lg text-green-dark font-semibold rounded-md hover:bg-green-main hover:text-primary-extra-light">
             <Link href="/"> Dashboard </Link>
           </li>
