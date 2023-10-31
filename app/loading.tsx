7a9b24226a68a30bdd26149fd4c2e2542e520e93
acalvom <andreac.3108@gmail.com>
2023-10-31 09:54:24 +0100

    feat: create loading component & delay function

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+  return (
+    <main className="flex flex-col m-16 items-center">
+      <h1 className="py-2 text-4xl text-green-dark font-black">Loading...</h1>
+      <h2 className="py-2 text-3xl text-green-main font-extrabold">
+        Patience is a conquering virtue 🧘‍♀️
+      </h2>
+    </main>
+  )
+}
